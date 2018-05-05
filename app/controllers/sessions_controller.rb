81e8bb37a7a20ee0f6421d01e64c9d91af7a974e
HoangDinh <dhoangk07@gmail.com>
2018-05-05 16:57:20 +0700

    skip_before_action for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize, only: [:new, :create]
   def new
   end
 
   def create
     user = User.find_by(name: params[:name])
-    if user and user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to admin_url
-    else
-      redirect_to login_url, alert: "Invalid user/password combination" end
     if user.present?
       if  user.sign_in_count >=3  
         redirect_to login_url, alert: "User has login more than 3 times" 
@@ -29,7 +25,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Logged out"
+    redirect_to root_path, notice: "Logged out"
   end
 
 end
