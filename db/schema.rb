f277e2e02087dffc6d90fcb4d5be8e7b273e8951
HoangDinh <dhoangk07@gmail.com>
2018-05-05 17:05:09 +0700

    Change field of users table from name to email.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_081727) do
+ActiveRecord::Schema.define(version: 2018_05_05_100204) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sign_in_count", default: 0
   end
-
 end
