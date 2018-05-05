27cf3f86a73346982bdf447156f95390a5e2a80f
HoangDinh <dhoangk07@gmail.com>
2018-05-05 16:55:37 +0700

    User can not login if login more than 3 times failed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,21 @@ class SessionsController < ApplicationController
       redirect_to admin_url
     else
       redirect_to login_url, alert: "Invalid user/password combination" end
+    if user.present?
+      if  user.sign_in_count >=3  
+        redirect_to login_url, alert: "User has login more than 3 times" 
+      else
+        if user.authenticate(params[:password])
+          user.sign_in_count = 0
+          user.save
+          session[:user_id] = user.id
+          redirect_to admin_url
+        else
+          user.sign_in_count += 1 
+          user.save
+          redirect_to login_url, alert: "Invalid user/password combination" 
+        end
+      end
     end
   end
 
